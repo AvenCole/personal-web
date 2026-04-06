a14cc1c61638f930ca1a2a8ac49fc3793b1c71f7
1IANZ <1587005702@qq.com>
2026-04-06 22:24:30 +0800

    refactor: remove AuroraBackground component from TechStacks view

--- src/views/TechStacks.vue
+++ src/views/TechStacks.vue
@@ -1,12 +1,10 @@
 <template>
-  <AuroraBackground class="min-h-[calc(100vh-4.5rem)] px-4 py-8 sm:px-6 lg:px-8">
-    <div class="mx-auto flex w-full max-w-6xl flex-col gap-8">
-      <TechStackGrid />
-    </div>
-  </AuroraBackground>
+  <div class="mx-auto flex w-full max-w-6xl flex-col gap-8 mt-10">
+    <TechStackGrid />
+  </div>
 </template>
 
 <script setup lang="ts">
-import AuroraBackground from '@/components/ui/aurora-background/AuroraBackground.vue'
+
 import TechStackGrid from '@/components/ui/tech-stacks/TechStackGrid.vue'
 </script>
